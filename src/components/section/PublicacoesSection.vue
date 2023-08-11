1c371e38a7966efb86337a9646163bb4ad598c26
Aldiran Santos <contact.aldiransantos@gmail.com>
2023-08-11 16:53:58 -0300

    :zap: perf: images optimization of publicacoes section

--- src/components/section/PublicacoesSection.vue
+++ src/components/section/PublicacoesSection.vue
@@ -6,15 +6,101 @@
 			artesanal.
 		</p>
 		<div class="galery">
-			<img
-				v-for="i in 8"
-				loading="lazy"
-				width="270"
-				height="218"
-				:src="require('@/assets/publicacoes/empire-burger-' + i + '.webp')"
-				:alt="'Empire Burger ' + i"
-				:key="'empire-burger-' + i"
-			/>
+			<picture class="picture">
+				<img
+					loading="lazy"
+					width="150"
+					height="219"
+					src="@/assets/publicacoes/empire-burger-desktop-1.webp"
+					alt="Empire Burger 1"
+				/>
+			</picture>
+
+			<picture class="picture">
+				<img
+					loading="lazy"
+					width="270"
+					height="219"
+					src="@/assets/publicacoes/empire-burger-desktop-2.webp"
+					alt="Empire Burger 2"
+				/>
+			</picture>
+
+			<picture class="picture">
+				<source
+					media="(max-width: 657.98px)"
+					srcset="@/assets/publicacoes/empire-burger-mobile-3.webp"
+				/>
+				<img
+					loading="lazy"
+					width="270"
+					height="219"
+					src="@/assets/publicacoes/empire-burger-desktop-3.webp"
+					alt="Empire Burger 3"
+				/>
+			</picture>
+
+			<picture class="picture">
+				<source
+					media="(max-width: 657.98px)"
+					srcset="@/assets/publicacoes/empire-burger-mobile-4.webp"
+				/>
+				<img
+					loading="lazy"
+					width="270"
+					height="219"
+					src="@/assets/publicacoes/empire-burger-desktop-4.webp"
+					alt="Empire Burger 4"
+				/>
+			</picture>
+
+			<picture class="picture">
+				<source
+					media="(max-width: 657.98px)"
+					srcset="@/assets/publicacoes/empire-burger-mobile-5.webp"
+				/>
+				<img
+					loading="lazy"
+					width="270"
+					height="219"
+					src="@/assets/publicacoes/empire-burger-desktop-5.webp"
+					alt="Empire Burger 5"
+				/>
+			</picture>
+
+			<picture class="picture">
+				<source
+					media="(max-width: 657.98px)"
+					srcset="@/assets/publicacoes/empire-burger-mobile-6.webp"
+				/>
+				<img
+					loading="lazy"
+					width="270"
+					height="219"
+					src="@/assets/publicacoes/empire-burger-desktop-6.webp"
+					alt="Empire Burger 6"
+				/>
+			</picture>
+
+			<picture class="picture">
+				<img
+					loading="lazy"
+					width="270"
+					height="219"
+					src="@/assets/publicacoes/empire-burger-desktop-7.webp"
+					alt="Empire Burger 7"
+				/>
+			</picture>
+
+			<picture class="picture">
+				<img
+					loading="lazy"
+					width="150"
+					height="219"
+					src="@/assets/publicacoes/empire-burger-desktop-8.webp"
+					alt="Empire Burger 8"
+				/>
+			</picture>
 		</div>
 		<div class="marquee"></div>
 	</section>
